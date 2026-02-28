f0cd0e5f0552dd8adb19c9c2eaf8732c83810798
Hiya Sarkar <sarkarhiya53@gmail.com>
2026-02-28 17:19:45 +0530

    Signup and Login page created and auth0 added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from "./pages/LandingPage.jsx"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from "./pages/About.jsx"
 import Contact from "./pages/Contact.jsx"
+import Signup from "./pages/SignUp.jsx"
+import Login from "./pages/Login.jsx"
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
           <Route path = "/" element = {<LandingPage/>}/>
           <Route path = "/about" element = {<About/>}/>
           <Route path = "/contact" element = {<Contact/>}/>
+          <Route path = "/signup" element = {<Signup/>}/>
+          <Route path = "/login" element = {<Login/>}/>
         </Routes>
       </Router>
     </>
